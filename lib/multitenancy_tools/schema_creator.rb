add60c3b49cfb4b25839df9103d5843aa37b5ec4
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-06 18:05:43 -0300

    Quote schema name and avoid sql injection

--- lib/multitenancy_tools/schema_creator.rb
+++ lib/multitenancy_tools/schema_creator.rb
@@ -7,8 +7,12 @@ module MultitenancyTools
 
     def from_sql(file)
       original_path = @connection.schema_search_path
-      @connection.execute("CREATE SCHEMA IF NOT EXISTS #{@schema}")
-      @connection.schema_search_path = @schema
+
+      quoted_for_search_path = @connection.quote(@schema)
+      quoted_schema_name = @connection.quote_table_name(@schema)
+
+      @connection.execute("CREATE SCHEMA IF NOT EXISTS #{quoted_schema_name}")
+      @connection.schema_search_path = quoted_for_search_path
       @connection.execute(File.read(file))
     ensure
       @connection.schema_search_path = original_path
