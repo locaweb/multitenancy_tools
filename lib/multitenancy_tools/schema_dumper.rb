42f1f6a128b9bed7229789787943ca5d0fb3927b
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-07 14:55:16 -0300

    Add documentation for SchemaDumper

--- lib/multitenancy_tools/schema_dumper.rb
+++ lib/multitenancy_tools/schema_dumper.rb
@@ -1,12 +1,33 @@
 require 'open3'
 
 module MultitenancyTools
+  # {SchemaDumper} can be used to generate SQL dumps of the structure of a
+  # PostgreSQL schema.
+  #
+  # The generated dump DOES NOT contain:
+  # * privilege statements (GRANT/REVOKE)
+  # * tablespace assigments
+  # * ownership information
+  # * any table data
+  #
+  # {SchemaDumper} is suitable to create SQL templates for {SchemaCreator}.
+  #
+  # @example
+  #   File.open('file.sql', 'w') do |f|
+  #     dumper = MultitenancyTools::SchemaDumper.new('my_db', 'my_schema')
+  #     dumper.dump_to(f)
+  #   end
   class SchemaDumper
+    # @param database [String] database name
+    # @param schema [String] schema name
     def initialize(database, schema)
       @database = database
       @schema = schema
     end
 
+    # Generates a dump an writes it into a File.
+    #
+    # @param file [File] the File object that will receive the dump
     def dump_to(file)
       stdout, stderr, status = Open3.capture3(
         'pg_dump',
