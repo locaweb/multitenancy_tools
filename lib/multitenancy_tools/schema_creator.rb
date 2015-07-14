7f648b0074a1dbac48e4e354c7109ccb075f8122
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-14 17:17:34 -0300

    Add documentation for MultitenancyTools::SchemaCreator

--- lib/multitenancy_tools/schema_creator.rb
+++ lib/multitenancy_tools/schema_creator.rb
@@ -1,10 +1,20 @@
 module MultitenancyTools
+  # {SchemaCreator} can be used to create a schema on a PostgreSQL database
+  # using a SQL file as template (this template should be previously generated
+  # using {SchemaDumper}).
+  #
+  # @example
+  #   creator = MultitenancyTools::SchemaCreator.new('my_schema', ActiveRecord::Base.connection)
+  #   creator.create_from_file('path/to/file.sql')
   class SchemaCreator
+    # @param schema [String] schema name
+    # @param connection [ActiveRecord::ConnectionAdapters::PostgreSQLAdapter] connection adapter
     def initialize(schema, connection)
       @schema = schema
       @connection = connection
     end
 
+    # @param file [String] path to a SQL file
     def create_from_file(file)
       quoted_schema_name = @connection.quote_table_name(@schema)
 
