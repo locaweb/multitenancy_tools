ce080267c73b39218475e6a0adf46b00adf43153
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-14 13:57:24 -0300

    Rewrite TableDumper to receive a file path and not an IO object

--- lib/multitenancy_tools/table_dumper.rb
+++ lib/multitenancy_tools/table_dumper.rb
@@ -24,7 +24,9 @@ module MultitenancyTools
         raise PgDumpError.new(stderr)
       end
 
-      file.write(DumpCleaner.new(stdout).clean)
+      File.open(file, 'w') do |f|
+        f.write DumpCleaner.new(stdout).clean
+      end
     end
   end
 end
--- spec/multitenancy_tools/table_dumper_spec.rb
+++ spec/multitenancy_tools/table_dumper_spec.rb
@@ -21,65 +21,73 @@ RSpec.describe MultitenancyTools::TableDumper do
     SQL
   end
 
-  subject do
-    described_class.new(Db.name, 'schema1', 'posts')
-  end
-
   describe '#dump_to' do
-    let(:io) { StringIO.new }
+    subject do
+      described_class.new(Db.name, 'schema1', 'posts')
+    end
+
+    around do |example|
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          example.run
+        end
+      end
+    end
 
     context 'table exists' do
       before do
-        subject.dump_to(io)
-        io.rewind
+        subject.dump_to('dump.sql')
       end
 
+      let(:file) { File.read('dump.sql') }
+
       it 'generates a SQL dump of the schema' do
-        expect(io.read).to eql(File.read('spec/fixtures/table_dump.sql'))
+        dump_file = File.expand_path('../../fixtures/table_dump.sql', __FILE__)
+        expect(file).to eql(File.read(dump_file))
       end
 
       it 'includes table data' do
-        expect(io.read).to match(/INSERT INTO/)
+        expect(file).to match(/INSERT INTO/)
       end
 
       it 'does not contain create table statements' do
-        expect(io.read).to_not match(/CREATE TABLE/)
+        expect(file).to_not match(/CREATE TABLE/)
       end
 
       it 'does not dump privileges' do
-        expect(io.read).to_not match(/GRANT|REVOKE/)
+        expect(file).to_not match(/GRANT|REVOKE/)
       end
 
       it 'does not dump tablespace assignments' do
-        expect(io.read).to_not match(/default_tablespace/)
+        expect(file).to_not match(/default_tablespace/)
       end
 
       it 'does not include object ownership' do
-        expect(io.read).to_not match(/OWNER TO/)
+        expect(file).to_not match(/OWNER TO/)
       end
 
       it 'does not include create schema statements' do
-        expect(io.read).to_not match(/CREATE SCHEMA/)
+        expect(file).to_not match(/CREATE SCHEMA/)
       end
 
       it 'does not set search_path' do
-        expect(io.read).to_not match(/SET search_path/)
+        expect(file).to_not match(/SET search_path/)
       end
 
       it 'does not set statement_timeout' do
-        expect(io.read).to_not match(/SET statement_timeout/)
+        expect(file).to_not match(/SET statement_timeout/)
       end
 
       it 'does not set lock_timeout' do
-        expect(io.read).to_not match(/SET lock_timeout/)
+        expect(file).to_not match(/SET lock_timeout/)
       end
 
       it 'does not include any comments' do
-        expect(io.read).to_not match(/--/)
+        expect(file).to_not match(/--/)
       end
 
       it 'removes duplicate line breaks' do
-        expect(io.read).to_not match(/\n\n/)
+        expect(file).to_not match(/\n\n/)
       end
     end
 
@@ -90,7 +98,7 @@ RSpec.describe MultitenancyTools::TableDumper do
 
       it 'raises an error' do
         expect do
-          subject.dump_to(io)
+          subject.dump_to('dump.sql')
         end.to raise_error(MultitenancyTools::PgDumpError,
                            /No matching tables were found/)
       end
