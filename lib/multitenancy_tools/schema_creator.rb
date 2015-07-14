c2dd6d6ed3aab7f093ff37879450f221ed4a975b
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-14 13:17:19 -0300

    Add an option to create schema or not

--- lib/multitenancy_tools/schema_creator.rb
+++ lib/multitenancy_tools/schema_creator.rb
@@ -5,11 +5,11 @@ module MultitenancyTools
       @connection = connection
     end
 
-    def from_sql(file)
+    def from_sql(file, create_schema: true)
       quoted_schema_name = @connection.quote_table_name(@schema)
 
       Tenant.new(@connection, @schema).run do
-        @connection.create_schema(quoted_schema_name)
+        @connection.create_schema(quoted_schema_name) if create_schema
         @connection.execute(File.read(file))
       end
     end
--- spec/multitenancy_tools/schema_creator_spec.rb
+++ spec/multitenancy_tools/schema_creator_spec.rb
@@ -3,26 +3,39 @@ require 'tempfile'
 
 RSpec.describe MultitenancyTools::SchemaCreator do
   let(:schema_name) { 'schema1' }
+  let(:sql_path) { 'spec/fixtures/schema_dump.sql' }
 
   subject do
     described_class.new(schema_name, Db.connection)
   end
 
   describe '#from_sql' do
-    before do
-      subject.from_sql('spec/fixtures/schema_dump.sql')
-    end
-
     after do
       Db.connection.execute("DROP SCHEMA IF EXISTS #{schema_name} CASCADE")
     end
 
     it 'executes the sql file' do
+      subject.from_sql(sql_path)
       expect(Db.connection.table_exists?("#{schema_name}.posts")).to be true
     end
 
     it 'sets connection search path back to its original value' do
+      subject.from_sql(sql_path)
       expect(Db.connection.schema_search_path).to eql('"$user",public')
+    end
+
+    context 'create_schema is false' do
+      it 'raises an error when schema does not exist' do
+        expect do
+          subject.from_sql(sql_path, create_schema: false)
+        end.to raise_error(/no schema has been selected to create in/)
+      end
+
+      it 'executes the sql file successfully when the schema exist' do
+        Db.connection.execute("CREATE SCHEMA #{schema_name}")
+        subject.from_sql(sql_path, create_schema: false)
+        expect(Db.connection.table_exists?("#{schema_name}.posts")).to be true
+      end
     end
   end
 end
