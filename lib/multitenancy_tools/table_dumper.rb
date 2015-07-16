19164bf68ee26e25164bceba16fdf83ad8099238
Lenon Marcel <lenon.marcel@gmail.com>
2015-07-16 17:17:11 -0300

    Add documentation for TableDumper

--- lib/multitenancy_tools/table_dumper.rb
+++ lib/multitenancy_tools/table_dumper.rb
@@ -1,13 +1,38 @@
 require 'open3'
 
 module MultitenancyTools
+  # {TableDumper} can be used to generate SQL dumps of the structure and,
+  # unlike {SchemaDumper}, the data of a PostgreSQL table. It requires pg_dump.
+  #
+  # The generated dump DOES NOT contain:
+  # * privilege statements (GRANT/REVOKE)
+  # * tablespace assigments
+  # * ownership information
+  #
+  # The dump will use INSERTs instead of COPY statements.
+  #
+  # @see http://www.postgresql.org/docs/9.3/static/app-pgdump.html
+  #      pg_dump
+  # @example
+  #   dumper = MultitenancyTools::TableDumper.new('db name', 'schema name', 'table name')
+  #   dumper.dump_to('path/to/file.sql')
   class TableDumper
+    # @param database [String] database name
+    # @param schema [String] schema name
+    # @param table [String] table name
     def initialize(database, schema, table)
       @database = database
       @schema = schema
       @table = table
     end
 
+    # Generates a dump an writes it into a file. Please see {IO.new} for open
+    # modes.
+    #
+    # @see http://ruby-doc.org/core-2.2.2/IO.html#method-c-new-label-Open+Mode
+    #      IO Open Modes
+    # @param file [String] file path
+    # @param mode [String] IO open mode
     def dump_to(file, mode: 'w')
       stdout, stderr, status = Open3.capture3(
         'pg_dump',
