0ccb3e1e8cea536d10d47b2451211048879e3118
Lenon Marcel <lenon.marcel@gmail.com>
2015-08-03 09:33:33 -0300

    Update documentation

--- lib/multitenancy_tools.rb
+++ lib/multitenancy_tools.rb
@@ -7,12 +7,12 @@ require 'multitenancy_tools/table_dumper'
 require 'multitenancy_tools/schema_switcher'
 
 module MultitenancyTools
-  # Uses the tenant to perform all queries executed in the block.
+  # Uses the passed schema as the scope for all queries triggered by the block.
   #
-  # @param tenant [String] schema name
+  # @param schema [String] schema name
   # @param connection [ActiveRecord::ConnectionAdapters::PostgreSQLAdapter] connection adapter
-  # @yield The block that must be executed using the tenant connection
-  def self.using(tenant, connection = ActiveRecord::Base.connection, &block)
-    SchemaSwitcher.new(tenant, connection).run(&block)
+  # @yield The block that must be executed using the schema as scope
+  def self.using(schema, connection = ActiveRecord::Base.connection, &block)
+    SchemaSwitcher.new(schema, connection).run(&block)
   end
 end
