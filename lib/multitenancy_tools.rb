80abdff519cbc39e38153363e8cdc8961777312d
Lenon Marcel <lenon.marcel@gmail.com>
2015-08-03 09:37:07 -0300

    Add a short hand method for schema creation

--- lib/multitenancy_tools.rb
+++ lib/multitenancy_tools.rb
@@ -7,6 +7,15 @@ require 'multitenancy_tools/table_dumper'
 require 'multitenancy_tools/schema_switcher'
 
 module MultitenancyTools
+  # Creates a new schema using the SQL file as template.
+  #
+  # @param name [String] schema name
+  # @param sql_file [String] absolute path to the SQL template
+  # @param connection [ActiveRecord::ConnectionAdapters::PostgreSQLAdapter] connection adapter
+  def self.create(name, sql_file, connection = ActiveRecord::Base.connection)
+    SchemaCreator.new(name, connection).create_from_file(sql_file)
+  end
+
   # Uses the passed schema as the scope for all queries triggered by the block.
   #
   # @param schema [String] schema name
--- spec/multitenancy_tools_spec.rb
+++ spec/multitenancy_tools_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe MultitenancyTools do
+  describe '.create' do
+    it 'creates a new schema using the SQL template' do
+      creator = double
+
+      expect(MultitenancyTools::SchemaCreator)
+        .to receive(:new)
+        .with('foobar', ActiveRecord::Base.connection) { creator }
+      expect(creator).to receive(:create_from_file).with('path/to/file.sql')
+
+      subject.create('foobar', 'path/to/file.sql')
+    end
+  end
+
   describe '.using' do
     it 'routes all queries to the passed tenant' do
       switcher = double
