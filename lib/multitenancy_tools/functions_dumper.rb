85fee45e59d55d44e9e1e61eed957f7bdcd26a0b
Lenon Marcel <lenon.marcel@gmail.com>
2015-08-17 18:10:06 -0300

    Rewrite FunctionsDumper to dump a valid SQL file

--- lib/multitenancy_tools/functions_dumper.rb
+++ lib/multitenancy_tools/functions_dumper.rb
@@ -6,18 +6,16 @@ module MultitenancyTools
     end
 
     def dump_to(file, mode: 'w')
-      query = <<-SQL
+      results = @connection.execute(<<-SQL)
         SELECT pg_get_functiondef(f.oid) as definition
         FROM pg_catalog.pg_proc f
         INNER JOIN pg_catalog.pg_namespace n ON (f.pronamespace = n.oid)
         WHERE n.nspname = #{@schema};
       SQL
 
-      results = @connection.execute(query)
-
       File.open(file, mode) do |f|
         results.each do |result|
-          f.write result['definition']
+          f.write result['definition'].strip + ";\n"
         end
       end
     end
--- spec/multitenancy_tools/functions_dumper_spec.rb
+++ spec/multitenancy_tools/functions_dumper_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 
 RSpec.describe MultitenancyTools::FunctionsDumper do
-  before do
+  before(:all) do
     Db.connection.execute(<<-SQL)
       DROP SCHEMA IF EXISTS schema1 CASCADE;
+      DROP SCHEMA IF EXISTS schema2 CASCADE;
       CREATE SCHEMA schema1;
-      CREATE TABLE schema1.posts (
-          id SERIAL NOT NULL PRIMARY KEY,
-          title text,
-          body text
-      );
-      INSERT INTO schema1.posts (title, body)
-      VALUES ('foo bar baz', 'post content');
-      CREATE FUNCTION schema1.cast_to_date(text) RETURNS date
-          LANGUAGE plpgsql IMMUTABLE
-          AS $_$
-              BEGIN
-                  RETURN CAST($1 AS DATE); EXCEPTION
-              WHEN data_exception THEN
-                  RETURN NULL ;
-              END ; $_$;
+      CREATE SCHEMA schema2;
+      CREATE FUNCTION schema1.increment(i integer) RETURNS integer AS $$
+          BEGIN
+              RETURN i + 1;
+          END;
+      $$ LANGUAGE plpgsql;
+      CREATE FUNCTION schema1.decrement(i integer) RETURNS integer AS $$
+          BEGIN
+              RETURN i - 1;
+          END;
+      $$ LANGUAGE plpgsql;
+      CREATE FUNCTION schema2.function_for_schema2(i integer) RETURNS integer AS $$
+          BEGIN
+              RETURN i + 1;
+          END;
+      $$ LANGUAGE plpgsql;
     SQL
   end
 
   describe '#dump_to' do
-    subject do
-      described_class.new('schema1', Db.connection)
-    end
+    subject { described_class.new('schema1', Db.connection) }
 
     around do |example|
       Dir.mktmpdir do |dir|
@@ -42,9 +42,14 @@ RSpec.describe MultitenancyTools::FunctionsDumper do
 
     let(:file) { File.read('dump.sql') }
 
-    it 'generates a SQL dump with all functions' do
+    it 'generates a SQL dump with all functions of the schema' do
       dump_file = File.expand_path('../../fixtures/functions_dump.sql', __FILE__)
       expect(file).to eql(File.read(dump_file))
+    end
+
+    it 'does not include functions from other schemas' do
+      expect(file).to_not match(/schema2/)
+      expect(file).to_not match(/function_for_schema2/)
     end
   end
 end
