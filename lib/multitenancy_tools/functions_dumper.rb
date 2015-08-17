f5c93dd60095a8336e5b75e7670e2a1c8f29e622
Lenon Marcel <lenon.marcel@gmail.com>
2015-08-17 18:15:30 -0300

    Add documentation for FunctionsDumper

--- lib/multitenancy_tools/functions_dumper.rb
+++ lib/multitenancy_tools/functions_dumper.rb
@@ -1,10 +1,25 @@
 module MultitenancyTools
+  # {FunctionsDumper} can be used to generate a SQL dump of all functions that
+  # are present on a PostgreSQL schema.
+  #
+  # @example
+  #   dumper = MultitenancyTools::FunctionsDumper.new('schema name')
+  #   dumper.dump_to('path/to/file.sql')
   class FunctionsDumper
+    # @param schema [String] schema name
+    # @param connection [ActiveRecord::ConnectionAdapters::PostgreSQLAdapter] connection adapter
     def initialize(schema, connection = ActiveRecord::Base.connection)
       @connection = connection
       @schema = @connection.quote(schema)
     end
 
+    # Generates a dump and writes it into a file. Please see {IO.new} for open
+    # modes.
+    #
+    # @see http://ruby-doc.org/core-2.2.2/IO.html#method-c-new-label-Open+Mode
+    #      IO Open Modes
+    # @param file [String] file path
+    # @param mode [String] IO open mode
     def dump_to(file, mode: 'w')
       results = @connection.execute(<<-SQL)
         SELECT pg_get_functiondef(f.oid) as definition
